d9a08660c40f214d96a36e17152db35d9ddb053a
Justin Cheung <jcheung@chegg.com>
2025-01-19 12:03:31 -0800

    [NEW] add api CRU endpoints for buildShipments and shipments

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,7 +1,19 @@
 import express from 'express'
 import cors from 'cors'
-import { createWorkspace, getWorkspace, getWorkspaces, updateWorkspace } from './util'
 import { reset } from './db/db'
+import {
+  createWorkspace,
+  createBuildShipment,
+  createShipment,
+  getWorkspaces,
+  getWorkspace,
+  getBuildShipment,
+  getShipment,
+  updateWorkspace,
+  updateBuildShipment,
+  updateShipment,
+} from './util'
+import { Shipment, BuildShipment } from './types'
 
 const app = express()
 app.use(cors())
@@ -11,18 +23,81 @@ const port = 8080
 const dbString = '../database.txt'
 
 /** Admin endpoint for resetting the database */
-app.get('/reset', (req, res) => {
+app.delete('/reset', (req, res) => {
   reset(dbString)
   res.send('Reset database')
 })
 
+/** Creates a new shipment in the database and returns it */
+app.post('/:workspaceId/buildShipments/:buildShipmentId/shipments', (req, res) => {
+  const shipment = createShipment(dbString, req.params.workspaceId, req.params.buildShipmentId)
+  res.json({ shipment })
+})
+
+/** Returns the shipment with the given ID */
+app.get('/:workspaceId/buildShipments/:buildShipmentId/shipments/:shipmentId', (req, res) => {
+  const shipment = getShipment(
+    dbString,
+    req.params.workspaceId,
+    req.params.buildShipmentId,
+    req.params.shipmentId
+  )
+  res.json({ shipment })
+})
+
+/** Updates the shipment with the given ID   */
+app.patch('/:workspaceId/buildShipments/:buildShipmentId/shipments/:shipmentId', (req, res) => {
+  const shipment = updateShipment(
+    dbString,
+    req.params.workspaceId,
+    req.params.buildShipmentId,
+    req.body.shipment
+  )
+  res.json({ shipment })
+})
+
+/** Creates a new buildShipment in the database and returns it */
+app.post('/:workspaceId/buildShipments', (req, res) => {
+  const buildShipment = createBuildShipment(dbString, req.params.workspaceId)
+  res.json({ buildShipment })
+})
+
+/** Returns the buildShipment with the given ID */
+app.get('/:workspaceId/buildShipments/:buildShipmentId', (req, res) => {
+  const buildShipment = getBuildShipment(
+    dbString,
+    req.params.workspaceId,
+    req.params.buildShipmentId
+  )
+  buildShipment.shipments = buildShipment.shipments.map((shipment) => ({
+    id: shipment.id,
+    orderNumber: shipment.orderNumber,
+  })) as Shipment[]
+  res.json({ buildShipment })
+})
+
+/** Updates the buildShipment with the given ID  */
+app.patch('/:workspaceId/buildShipments/:buildShipmentId', (req, res) => {
+  const buildShipment = updateBuildShipment(
+    dbString,
+    req.params.workspaceId,
+    req.body.buildShipment
+  )
+  res.json({ buildShipment })
+})
+
 /** Returns the workspace with the given ID */
 app.get('/:workspaceId', (req, res) => {
-  res.json({ workspace: getWorkspace(dbString, req.params.workspaceId) })
+  const workspace = getWorkspace(dbString, req.params.workspaceId)
+  workspace.buildShipments = workspace.buildShipments.map((buildShipment) => ({
+    id: buildShipment.id,
+    buildNumber: buildShipment.buildNumber,
+  })) as BuildShipment[]
+  res.json({ workspace })
 })
 
 /** Updates the workspace with the given ID and returns the updated workspace */
-app.post('/:workspaceId', (req, res) => {
+app.patch('/:workspaceId', (req, res) => {
   const workspace = req.body.workspace
   res.json({ workspace: updateWorkspace(dbString, workspace) })
 })
