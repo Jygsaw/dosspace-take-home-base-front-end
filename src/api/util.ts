31188b5817b0f08906ea75740af9126f3d2b6d11
Justin Cheung <jcheung@chegg.com>
2025-01-19 17:49:32 -0800

    [NEW] add api CRU utils for buildShipments and shipments

--- src/api/util.ts
+++ src/api/util.ts
@@ -1,6 +1,25 @@
 import { all, findOne, insert, update } from './db/db'
-import { Workspace } from './types'
+import { BuildShipment, Shipment, Workspace } from './types'
 import { v4 as uuidv4 } from 'uuid'
+
+const buildDefaultWorkspace = (): Workspace => ({
+  id: uuidv4(),
+  title: '',
+  buildShipments: [buildDefaultBuildShipment()],
+})
+
+const buildDefaultBuildShipment = (): BuildShipment => ({
+  id: uuidv4(),
+  buildNumber: '',
+  shipments: [buildDefaultShipment()],
+})
+
+const buildDefaultShipment = (): Shipment => ({
+  id: uuidv4(),
+  description: '',
+  orderNumber: '',
+  cost: 0,
+})
 
 /** Returns a list of all workspaces in the database */
 export function getWorkspaces(dbString: string): Workspace[] {
@@ -14,24 +33,116 @@ export function getWorkspace(dbString: string, id: string): Workspace {
 
 /** Create a workspace in the database */
 export function createWorkspace(dbString: string): Workspace {
-  const workspace: Workspace = {
-    id: uuidv4(),
-    title: '',
-    buildShipments: [
-      {
-        id: uuidv4(),
-        buildNumber: '',
-        // Initialize the workspace with a single empty build shipment
-        shipments: [{ id: uuidv4(), description: '', orderNumber: '', cost: 0 }],
-      },
-    ],
-  }
+  const workspace = buildDefaultWorkspace()
   insert(dbString, 'workspaces', workspace)
-  return workspace
+  return getWorkspace(dbString, workspace.id)
 }
 
 /** Update a workspace in the database */
-export function updateWorkspace(dbString: string, workspace: Workspace): Workspace {
-  update(dbString, 'workspaces', workspace.id, workspace)
-  return findOne(dbString, 'workspaces', workspace.id)
+export function updateWorkspace(
+  dbString: string,
+  workspace: Partial<Workspace> & { id: string }
+): Workspace {
+  const updatedWorkspace = {
+    ...getWorkspace(dbString, workspace.id),
+    ...workspace,
+  }
+  update(dbString, 'workspaces', updatedWorkspace.id, updatedWorkspace)
+  return getWorkspace(dbString, updatedWorkspace.id)
+}
+
+/** Returns a single buildShipment from the database */
+export function getBuildShipment(
+  dbString: string,
+  workspaceId: string,
+  buildShipmentId: string
+): BuildShipment {
+  const workspace = getWorkspace(dbString, workspaceId)
+  const buildShipment = workspace.buildShipments.find(
+    (buildShipment) => buildShipment.id === buildShipmentId
+  )
+  if (!buildShipment) {
+    throw new Error('Could not find item with id "' + buildShipmentId + '"')
+  }
+  return buildShipment
+}
+
+/** Create a buildShipment in the database */
+export function createBuildShipment(dbString: string, workspaceId: string): BuildShipment {
+  const buildShipment = buildDefaultBuildShipment()
+  const workspace = getWorkspace(dbString, workspaceId)
+  workspace.buildShipments.push(buildShipment)
+  updateWorkspace(dbString, workspace)
+  return getBuildShipment(dbString, workspaceId, buildShipment.id)
+}
+
+/** Update a buildShipment in the database */
+export function updateBuildShipment(
+  dbString: string,
+  workspaceId: string,
+  buildShipment: Partial<BuildShipment> & { id: string }
+): BuildShipment {
+  const updatedWorkspace = getWorkspace(dbString, workspaceId)
+  const buildShipmentIndex = updatedWorkspace.buildShipments.findIndex(
+    (buildShipmentElem) => buildShipmentElem.id === buildShipment.id
+  )
+  if (buildShipmentIndex === -1) {
+    throw new Error('Could not find item with id "' + buildShipment.id + '"')
+  }
+  updatedWorkspace.buildShipments[buildShipmentIndex] = {
+    ...updatedWorkspace.buildShipments[buildShipmentIndex],
+    ...buildShipment,
+  }
+  updateWorkspace(dbString, updatedWorkspace)
+  return getBuildShipment(dbString, workspaceId, buildShipment.id)
+}
+
+/** Returns a single shipment from the database */
+export function getShipment(
+  dbString: string,
+  workspaceId: string,
+  buildShipmentId: string,
+  shipmentId: string
+): Shipment {
+  const buildShipment = getBuildShipment(dbString, workspaceId, buildShipmentId)
+  const shipment = buildShipment.shipments.find((shipment) => shipment.id === shipmentId)
+  if (!shipment) {
+    throw new Error('Could not find item with id "' + shipmentId + '"')
+  }
+  return shipment
+}
+
+/** Create a shipment in the database */
+export function createShipment(
+  dbString: string,
+  workspaceId: string,
+  buildShipmentId: string
+): Shipment {
+  const shipment: Shipment = buildDefaultShipment()
+  const buildShipment = getBuildShipment(dbString, workspaceId, buildShipmentId)
+  buildShipment.shipments.push(shipment)
+  updateBuildShipment(dbString, workspaceId, buildShipment)
+  return getShipment(dbString, workspaceId, buildShipmentId, shipment.id)
+}
+
+/** Update a shipment in the database */
+export function updateShipment(
+  dbString: string,
+  workspaceId: string,
+  buildShipmentId: string,
+  shipment: Partial<Shipment> & { id: string }
+): Shipment {
+  const updatedBuildShipment = getBuildShipment(dbString, workspaceId, buildShipmentId)
+  const shipmentIndex = updatedBuildShipment.shipments.findIndex(
+    (shipmentElem) => shipmentElem.id === shipment.id
+  )
+  if (shipmentIndex === -1) {
+    throw new Error('Could not find item with id "' + shipment.id + '"')
+  }
+  updatedBuildShipment.shipments[shipmentIndex] = {
+    ...updatedBuildShipment.shipments[shipmentIndex],
+    ...shipment,
+  }
+  updateBuildShipment(dbString, workspaceId, updatedBuildShipment)
+  return getShipment(dbString, workspaceId, buildShipmentId, shipment.id)
 }
